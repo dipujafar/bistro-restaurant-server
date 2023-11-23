fc370f0b29a96cd1ee77cd7d5789bfa0732be603
dipujafar <dipujafarmr@gmail.com>
2023-11-24 00:56:06 +0600

    added manage and update menu api

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 const verifyToken = (req, res, next)=>{
-  console.log("inside verify token",req.headers.authorization);
   if(!req.headers.authorization){
     return res.status(401).send({message: "unathorized access"});
   };
@@ -79,6 +78,26 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/menu/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.post('/menu', verifyToken, verifyAdmin, async (req, res)=>{
+      const item = req.body;
+      const result = await menuCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async(req,  res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
+
     app.get('/reviews', async (req, res) => {
       const result = await reviewsCollection.find().toArray();
       res.send(result);
@@ -92,7 +111,6 @@ async function run() {
 
     app.post('/users', async (req, res) => {
       const user = req.body;
-      console.log(user);
 
       const query = { email: user.email }
       const existingUser = await userCollection.findOne(query);
